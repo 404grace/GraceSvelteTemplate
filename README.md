722d17311fc370d9f7ab95ad950dd568bfcf26f4
404grace <91774849+404grace@users.noreply.github.com>
2024-10-02 16:45:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Uporaba
+
+1. Inštaliraj projekt:
+```
+pnpm i
+```
+
+2. Zaženi projekt:
+```
+pnpm dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="grid justify-items-center gap-5">
+	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
+		Hello World, Hello Grace!
+	</h1>
+	<img class="rounded-md w-96" src="art.webp" />
+</div>
